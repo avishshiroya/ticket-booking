a8ee5b92f83502809ee9eff58258870470fab81e
avishshiroya <avishshiroya.crawlapps@gmail.com>
2024-04-04 09:07:11 +0530

    feat : initialize the middleware in other file

---
+++ middleware/middlewares.js
@@ -0,0 +1,19 @@
+import morgan from "morgan";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+import dotenv from "dotenv";
+import cors from "cors";
+import loggerPrint from "../utils/printLogger.js";
+import apicache from "apicache";
+import express from 'express'
+export default (app) => {
+  dotenv.config();
+  const cache = apicache.middleware;
+  app.use(express.json());
+  // app.use(bodyParser.json())
+  app.use(morgan("dev"));
+  app.use(loggerPrint);
+  app.use(cors());
+  app.use(cookieParser());
+  app.use(cache("5 minutes"));
+};
--- server.js
+++ server.js
@@ -1,85 +1,61 @@
-import express from "express"
-import morgan from "morgan"
-import bodyParser from "body-parser"
-import cookieParser from "cookie-parser"
-import dotenv from "dotenv"
-import fs from "fs"
-import paypal from "paypal-rest-sdk"
-import path from "path"
-import winston from "winston"
-import cors from "cors"
-import loggerPrint from "./utils/printLogger.js"
-import apicache from "apicache"
-import cluster from "cluster"
-import os from 'os';
-import connectDb from "./config/db.js"
-import Routes from "./routes/index.js"
-import { log } from "console"
+import express from "express";
+import paypal from "paypal-rest-sdk";
+import path from "path";
+import winston from "winston";
+// import loggerPrint from "./utils/printLogger.js";
+import cluster from "cluster";
+import os from "os";
+import connectDb from "./config/db.js";
+import Routes from "./routes/index.js";
+import middlewares from "./middleware/middlewares.js";
 // import logger from "./utils/logger.js"
 const totalCPUs = os.cpus().length;
 console.log(totalCPUs);
 const app = express();
 
-if(cluster.isPrimary){
-    console.log(`Primary ${process.pid} is running`);
+if (cluster.isPrimary) {
+  console.log(`Primary ${process.pid} is running`);
 
-    for(let i=0;i<totalCPUs;i++){
-        cluster.fork();
-    }
-
-}
-else{
-dotenv.config();
-
-paypal.configure({
+  for (let i = 0; i < totalCPUs; i++) {
+    cluster.fork();
+  }
+} else {
+  paypal.configure({
     mode: process.env.PAYPAL_MODE,
     client_id: process.env.PAYPAL_CLIENT_ID,
-    client_secret: process.env.PAYPAL_CLIENT_SECRET
-})
+    client_secret: process.env.PAYPAL_CLIENT_SECRET,
+  });
+  middlewares(app)
+  // app.use('/', express.static(path.join('public')));
+  // app.use(morgan('[:date[clf]] :method :url :status :response-time ms - :res[content-length]', { stream: accessLogStream }))
+  //connectDB
+  connectDb();
 
-const cache = apicache.middleware
+  // to save the logger in the text file *********
+  // var accessLogStream = fs.createWriteStream(`loggers/${todayDate.getDate()}_${todayDate.getMonth()}_${todayDate.getFullYear()}_access.log`, { flags: 'a' }) // create a write stream (in append mode)
+  // console.log(accessLogStream)
+  // app.use(morgan('[:date[clf]] :method :url :status :response-time ms - :res[content-length]', { stream: accessLogStream }))
+  //home routes
 
-app.use(express.json())
-// app.use(bodyParser.json())
-app.use(morgan("dev"))
-app.use(loggerPrint)
-app.use(cors())
-app.use(cookieParser())
-app.use(cache('5 minutes'))
-// app.use('/', express.static(path.join('public'))); 
-// app.use(morgan('[:date[clf]] :method :url :status :response-time ms - :res[content-length]', { stream: accessLogStream }))
-//connectDB
-connectDb()
+  //routes
 
+  app.use("/api/v1", Routes);
 
-// to save the logger in the text file *********
-// var accessLogStream = fs.createWriteStream(`loggers/${todayDate.getDate()}_${todayDate.getMonth()}_${todayDate.getFullYear()}_access.log`, { flags: 'a' }) // create a write stream (in append mode)
-// console.log(accessLogStream)
-// app.use(morgan('[:date[clf]] :method :url :status :response-time ms - :res[content-length]', { stream: accessLogStream }))
-//home routes
-
-//routes
-
-
-app.use("/api/v1", Routes)
-
-app.get("/", async (req, res) => {
+  app.get("/", async (req, res) => {
     res.status(200).json({
-        success: true,
-        message: "First Routes 214 214  " + process.pid
-    })
+      success: true,
+      message: "First Routes 214 214  " + process.pid,
+    });
     // res.redirect('./public/index.html');
+  });
 
-})
-
-const PORT = process.env.PORT || 4040
-app.listen(PORT, (err, res) => {
+  const PORT = process.env.PORT || 4040;
+  app.listen(PORT, (err, res) => {
     if (err) {
-        console.log(err);
+      console.log(err);
     } else {
-        console.log(`Port ${PORT} run successfully ${process.pid}`)
+      console.log(`Port ${PORT} run successfully ${process.pid}`);
     }
-})
-
+  });
 }
-export default app
+export default app;
